8ec6020edda8ca31301300c92b6927392c21ca3f
eondula <leezmiriam@gmail.com>
2021-02-17 19:15:53 -0800

    added custom env template

--- campus_gym/envs/__init__.py
+++
@@ -1,2 +0,0 @@
-from gym.envs.registration import register
-register(id='CampusEnv-v0', entry_point='envs.campus_env_dir:CampusEnv')
--- campus_gym/envs/campus_env_dir/campus_env.py
+++
@@ -1,8 +0,0 @@
-import gym
-class CampusEnv(gym.Env):
-    def __init__(self):
-        print('Environment initialized')
-    def step(self):
-        print('Step successful')
-    def reset(self):
-        pring('Environment reset')
