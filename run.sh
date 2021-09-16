9505cebe807d00a9bd27d19e89568eb09e7bcbfb
eondula <leezmiriam@gmail.com>
2021-09-16 12:43:14 -0700

    added job submission script

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-# run program X times consecutively
-seq 5 | xargs -Iz python3 main.py
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=5
+#SBATCH --cpus-per-task=8
+#SBATCH --job-name=LizardA.job
+#SBATCH --output=.out/LizardA.out
+#SBATCH --error=.out/LizardB.err
+#SBATCH --time=2-00:00
+#SBATCH --mem=12000
+#SBATCH --qos=normal
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=leezmiriam@gmail.com
+
+python3 main.py
