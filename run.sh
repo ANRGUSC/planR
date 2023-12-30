088f310d368d656fc156889a361ddfafa9ef110c
Sandeep Zechariah George <sandeepzgk@gmail.com>
2023-12-29 19:43:10 -0800

    Conda ENV, Updated Run.sh and requirements.txt
    created a conda environment for easier setup, updated run.sh to run "8" parallel alphas, and updated requirements.txt for latest versions that works

--- run.sh
+++ run.sh
@@ -3,13 +3,10 @@
 # Define the list of alpha values
 alphas=(0.01 0.02 0.1 0.25 0.35 0.41 0.42 0.43 0.44 0.45 0.46 0.47 0.48 0.49 0.5 0.6 0.7 0.8 0.9 1.0)
 
-# Loop through each alpha value
-for alpha in "${alphas[@]}"
-do
-    echo "Running training with alpha = $alpha"
-    python main.py train --alpha $alpha
-    # Optionally, you can redirect output to a file
-    # python main.py train --alpha $alpha > "output_alpha_$alpha.txt"
-done
+# Export main.py so it is available to subshells
+export -f main.py
+
+# Run tasks in parallel
+printf "%s\n" "${alphas[@]}" | xargs -I {} -P 8 bash -c 'echo "Running training with alpha = {}"; python main.py train --alpha {}'
 
 echo "All training runs completed."
