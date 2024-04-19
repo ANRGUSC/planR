1dc7b6ecfcb1aa2d26d0403b29ff25ae399f204c
eondula <leezmiriam@gmail.com>
2024-04-19 08:30:31 -0700

    kinda workd

--- models/infection_model.py
+++ models/infection_model.py
@@ -62,71 +62,69 @@ def calculate_indoor_infection_prob(room_capacity: int, initial_infection_prob: 
     return total_transmission_prob
 
 
-def get_infected_students(current_infected_students: list, allowed_students_per_course: list,
-                          students_per_course: list, initial_infection: list, community_risk: float):
-    infected_students = []
-    for n, f in enumerate(allowed_students_per_course):
-        if f == 0:
-            infected_students.append(int(community_risk * allowed_students_per_course[n]))
-        else:
-            asymptomatic_ratio = 0.5
-            initial_infection_prob = (current_infected_students[n] / (students_per_course[n])) * asymptomatic_ratio
-            print("initial infection prob: ", initial_infection_prob)
-            room_capacity = allowed_students_per_course[n]
-            infected_prob = calculate_indoor_infection_prob(room_capacity, initial_infection_prob)
-            print("infected prob: ", infected_prob, "allowed students per course: ", allowed_students_per_course[n])
-            total_indoor_infected_allowed = int(infected_prob * allowed_students_per_course[n])
-            total_infected_allowed_outdoor = int(community_risk * allowed_students_per_course[n])
-            total_infected_allowed = min(total_indoor_infected_allowed + total_infected_allowed_outdoor,
-                                         allowed_students_per_course[n])
-            print("total infected allowed: ", total_infected_allowed)
+# def get_infected_students(current_infected_students: list, allowed_students_per_course: list,
+#                           students_per_course: list, initial_infection: list, community_risk: float):
+#     infected_students = []
+#     for n, f in enumerate(allowed_students_per_course):
+#         if f == 0:
+#             infected_students.append(int(community_risk * allowed_students_per_course[n]))
+#         else:
+#             asymptomatic_ratio = 0.5
+#             initial_infection_prob = (current_infected_students[n] / (students_per_course[n])) * asymptomatic_ratio
+#             print("initial infection prob: ", initial_infection_prob)
+#             room_capacity = allowed_students_per_course[n]
+#             infected_prob = calculate_indoor_infection_prob(room_capacity, initial_infection_prob)
+#             print("infected prob: ", infected_prob, "allowed students per course: ", allowed_students_per_course[n])
+#             total_indoor_infected_allowed = int(infected_prob * allowed_students_per_course[n])
+#             total_infected_allowed_outdoor = int(community_risk * allowed_students_per_course[n])
+#             total_infected_allowed = min(total_indoor_infected_allowed + total_infected_allowed_outdoor,
+#                                          allowed_students_per_course[n])
+#             print("total infected allowed: ", total_infected_allowed)
+#
+#             infected_students.append(
+#                 int(round((total_infected_allowed) + (
+#                             community_risk * (students_per_course[n] - allowed_students_per_course[n])))))
+#
+#     return infected_students
 
-            infected_students.append(
-                int(round((total_infected_allowed) + (
-                            community_risk * (students_per_course[n] - allowed_students_per_course[n])))))
+def get_infected_students_apprx_sir(current_infected, allowed_per_course, community_risk, const_1, const_2, const_3):
 
-    return infected_students
+    current_infected = np.array(current_infected)
+    allowed_per_course = np.array(allowed_per_course)
 
-def get_infected_students_apprx_sir(current_infected, allowed_per_course, community_risk, const_1, const_2):
     # Simple approximation model that utilizes the community risk
-    infected_students = []
-    for i in range(len(allowed_per_course)):
-        # const_1 = 0.005  # reduce this to a smaller value
-        # const_2 = 0.01  # reduce this value to be very small 0.01, 0.02
 
-        prop_allowed = allowed_per_course[i] / 100
-        prop_infected = current_infected[i] / 100
-        prop_uninfected = 1 - prop_infected
+    susceptible = 100 - current_infected
+    prop_allowed = allowed_per_course / 100
+    prop_infected = current_infected / 100
 
-        susceptible = allowed_per_course[i] * prop_uninfected
-        uninfected = 100 - current_infected[i]
+    outside_risk = const_2 * community_risk
+    susceptible_out_class = susceptible * (1 - prop_allowed)
+    infected_out_class = outside_risk * susceptible_out_class
 
-        infected_in_class = const_1 * prop_allowed * prop_infected * susceptible
-        infected_out_class = const_2 * community_risk * prop_uninfected * uninfected
+    class_risk = const_1 * (prop_infected * prop_allowed) ** .5
+    total_risk = np.minimum(outside_risk + class_risk, 1.)
+    susceptible_in_class = susceptible * prop_allowed
+    infected_in_class = total_risk * susceptible_in_class
 
-        # Calculate the number of newly infected students
-        new_infected = int(infected_in_class + infected_out_class)
+    # Calculate the number of newly infected students
+    new_infected = (infected_in_class + infected_out_class).astype(np.int64)
 
-        # Calculate the number of recovered students
-        recovered = int(1.0 * current_infected[i])
+    # Calculate the number of recovered students
+    recovered = (const_3 * current_infected).astype(np.int64)
 
-        # Update the current infected count by adding new infections and subtracting recoveries
-        infected = max(min(current_infected[i] - recovered + new_infected, 100),0)
+    # Update the current infected count by adding new infections and subtracting recoveries
+    infected_students = current_infected - recovered + new_infected
 
-        infected_students.append(infected)
-    # print(infected_students)
-    # exit()
-
-    infected_students = list(map(int, list(map(round, infected_students))))
-    return infected_students
+    return list(infected_students)
 
 def get_infected_students_sir(current_infected, allowed_per_course, community_risk):
     # Simple approximation model that utilizes the community risk
 
     infected_students = []
     for i in range(len(allowed_per_course)):
-        const_1 = 0.005  # reduce this to a smaller value
-        const_2 = 0.01  # reduce this value to be very small 0.01, 0.02
+        const_1 = 0.005
+        const_2 = 0.01
         infected = int(((const_1 * current_infected[i]) * (allowed_per_course[i])) + (
                 (const_2 * community_risk) * allowed_per_course[i] ** 2))
 
@@ -136,161 +134,140 @@ def get_infected_students_sir(current_infected, allowed_per_course, community_ri
 
     infected_students = list(map(int, list(map(round, infected_students))))
     return infected_students
-def get_infected_students_sir_a(current_infected, allowed_per_course, community_risk):
-    # Simple approximation model that utilizes the community risk
-    infected_students = []
-    for i in range(len(allowed_per_course)):
-        const_1 = 0.005  # reduce this to a smaller value
-        const_2 = 0.01  # reduce this value to be very small 0.01, 0.02
-        susceptible = allowed_per_course[i] * (1 - (current_infected[i] / 100)) # 100 is total students
-        # Calculate the number of newly infected students
-        new_infected = int(((const_1 * current_infected[i]) * (allowed_per_course[i])) + (
-                (const_2 * community_risk) * susceptible) * susceptible)
 
-        # Calculate the number of recovered students
-        recovered = int(1.0 * current_infected[i])
-
-        # Update the current infected count by adding new infections and subtracting recoveries
-        infected = min(current_infected[i] + new_infected - recovered, allowed_per_course[i])
-
-        infected_students.append(infected)
-
-    infected_students = list(map(int, list(map(round, infected_students))))
-    return infected_students
-
-# Create a table
-table = []
-
-def generate_data(current_infected_list, allowed_per_course_values, community_risk_values):
-    if not current_infected_list:
-        return
-
-    current_infected = current_infected_list[0]
-
-    for allowed_per_course, community_risk in itertools.product(allowed_per_course_values, community_risk_values):
-        # infected_students = get_infected_students_sir([current_infected], [allowed_per_course], community_risk)
-        # infected_students = get_infected_students_sir([current_infected], [allowed_per_course], community_risk)
-        # infected_students = get_infected_students([current_infected], [allowed_per_course], [100], [], community_risk)
-        infected_students = get_infected_students_apprx_sir([current_infected], [allowed_per_course], community_risk, 0.005, 0.01)
-
-        row = (current_infected, allowed_per_course, community_risk, infected_students[0])
-        table.append(row)
-
-        # Update the current_infected for the next iteration
-        current_infected = infected_students[0]
-
-    # Recursively call the function with the next values in the list
-    generate_data(current_infected_list[1:], allowed_per_course_values, community_risk_values)
-
-# Define lists for current_infected, allowed_per_course, and community_risk
-initial_current_infected_list = [20]
-# allowed_per_course_values = [0, 50, 100]
-allowed_per_course_values = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-# community_risk_values = [0.01 + i * 0.005 for i in range(15)]  # 15 values from 0.01 to 0.1 in increments of 0.005
-community_risk_values = [0.1 + i * 0.06 for i in range(15)]
-# Start the recursive data generation
-generate_data(initial_current_infected_list, allowed_per_course_values, community_risk_values)
-
-# Create a PrettyTable object with column headers
-table_obj = PrettyTable()
-table_obj.field_names = ["Current Infected", "Allowed per Course", "Community Risk", "Infected Students"]
-
-# Populate the table with data
-for row in table:
-    table_obj.add_row(row)
-
-# Set the alignment for numeric columns (optional)
-table_obj.align["Current Infected"] = "r"
-table_obj.align["Allowed per Course"] = "r"
-table_obj.align["Community Risk"] = "r"
-table_obj.align["Infected Students"] = "r"
-
-# Print the table
-table_str = table_obj.get_string()
-with open("table-indoor-model-10act.txt", "w") as file:
-    file.write(table_str)
-
-# Extract the data from the table
-current_infected = [row[0] for row in table]
-allowed_per_course = [row[1] for row in table]
-community_risk = [row[2] for row in table]
-infected_students = [row[3] for row in table]
-
-# Create a 3D scatter plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-# Scatter plot with allowed_per_course as the color
-scatter = ax.scatter(community_risk, allowed_per_course, infected_students, c=allowed_per_course, cmap='tab20b')
-
-# Add labels and title
-ax.set_xlabel('Community Risk')
-ax.set_ylabel('Allowed per Course')
-ax.set_zlabel('Infected Students')
-plt.title('10-action Model behavior')
-
-# # Add a legend with custom entries at the bottom of the plot
-# legend_labels = ['Allow no one', '50% allowed', 'Allow everyone']
-# legend_colors = [scatter.cmap(scatter.norm(value)) for value in (0, 50, 100)]
-# custom_legend = [plt.Line2D([0], [0], ls="", marker='o',
-#                             markerfacecolor=color, markersize=10,
-#                             markeredgecolor="none")
-#                  for color in legend_colors]
-# ax.legend(custom_legend, legend_labels, loc='lower right', bbox_to_anchor=(0.5, -0.15), ncol=3, title="Allowed per Course")
-# plt.tight_layout()
-
-# legend1 = ax.legend(*scatter.legend_elements(), loc='upper left', ncol=3, title="Allowed per Course")
-# ax.add_artist(legend1)
-# Add colorbar to show the mapping of colors to allowed_per_course values
-# colorbar = plt.colorbar(scatter, label='Allowed per Course')
-
-# Save the figure
-plt.savefig('10act-3d-scatter-si-model-uai.png')
-
-
+# # Create a table
+# table = []
 #
-# # Prepare data
-# current_infected_data = [row[0] for row in table]
-# allowed_per_course_data = [row[1] for row in table]
-# community_risk_data = [row[2] for row in table]
-# infected_students_data = [row[3] for row in table]
+# def generate_data(current_infected_list, allowed_per_course_values, community_risk_values):
+#     if not current_infected_list:
+#         return
+#
+#     current_infected = current_infected_list[0]
+#
+#     for allowed_per_course, community_risk in itertools.product(allowed_per_course_values, community_risk_values):
+#         # infected_students = get_infected_students_sir([current_infected], [allowed_per_course], community_risk)
+#         # infected_students = get_infected_students_sir([current_infected], [allowed_per_course], community_risk)
+#         # infected_students = get_infected_students([current_infected], [allowed_per_course], [100], [], community_risk)
+#         infected_students = get_infected_students_apprx_sir([current_infected], [allowed_per_course], community_risk, 2., .8, 1.)
+#
+#         row = (current_infected, allowed_per_course, community_risk, infected_students[0])
+#         table.append(row)
+#
+#         # Update the current_infected for the next iteration
+#         current_infected = infected_students[0]
+#
+#     # Recursively call the function with the next values in the list
+#     generate_data(current_infected_list[1:], allowed_per_course_values, community_risk_values)
+#
+# # Define lists for current_infected, allowed_per_course, and community_risk
+# initial_current_infected_list = [20]
+# # allowed_per_course_values = [0, 50, 100]
+# allowed_per_course_values = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# # community_risk_values = [0.01 + i * 0.005 for i in range(15)]  # 15 values from 0.01 to 0.1 in increments of 0.005
+# community_risk_values = [0.1 + i * 0.06 for i in range(15)]
+# # Start the recursive data generation
+# generate_data(initial_current_infected_list, allowed_per_course_values, community_risk_values)
+#
+# # Create a PrettyTable object with column headers
+# table_obj = PrettyTable()
+# table_obj.field_names = ["Current Infected", "Allowed per Course", "Community Risk", "Infected Students"]
+#
+# # Populate the table with data
+# for row in table:
+#     table_obj.add_row(row)
+#
+# # Set the alignment for numeric columns (optional)
+# table_obj.align["Current Infected"] = "r"
+# table_obj.align["Allowed per Course"] = "r"
+# table_obj.align["Community Risk"] = "r"
+# table_obj.align["Infected Students"] = "r"
+#
+# # Print the table
+# table_str = table_obj.get_string()
+# with open("table-indoor-model-10act.txt", "w") as file:
+#     file.write(table_str)
+#
+# # Extract the data from the table
+# current_infected = [row[0] for row in table]
+# allowed_per_course = [row[1] for row in table]
+# community_risk = [row[2] for row in table]
+# infected_students = [row[3] for row in table]
 #
 # # Create a 3D scatter plot
 # fig = plt.figure()
 # ax = fig.add_subplot(111, projection='3d')
 #
-# # Scatter plot
-# scatter = ax.scatter(current_infected_data, allowed_per_course_data, community_risk_data,
-#                      c=infected_students_data, cmap='plasma', marker='o')
+# # Scatter plot with allowed_per_course as the color
+# scatter = ax.scatter(community_risk, allowed_per_course, infected_students, c=allowed_per_course, cmap='tab20b')
 #
-# # Adding labels and title
-# ax.set_xlabel('Current Infected')
-# ax.set_ylabel('Allowed Per Course')
-# ax.set_zlabel('Community Risk')
-# plt.title('Infected Students based on approximated sir model')
+# # Add labels and title
+# ax.set_xlabel('Community Risk')
+# ax.set_ylabel('Allowed per Course')
+# ax.set_zlabel('Infected Students')
+# plt.title('10-action Model behavior')
 #
-# # Adding a color bar
-# cbar = plt.colorbar(scatter, ax=ax)
-# cbar.set_label('Infected Students')
+# # # Add a legend with custom entries at the bottom of the plot
+# # legend_labels = ['Allow no one', '50% allowed', 'Allow everyone']
+# # legend_colors = [scatter.cmap(scatter.norm(value)) for value in (0, 50, 100)]
+# # custom_legend = [plt.Line2D([0], [0], ls="", marker='o',
+# #                             markerfacecolor=color, markersize=10,
+# #                             markeredgecolor="none")
+# #                  for color in legend_colors]
+# # ax.legend(custom_legend, legend_labels, loc='lower right', bbox_to_anchor=(0.5, -0.15), ncol=3, title="Allowed per Course")
+# # plt.tight_layout()
+#
+# # legend1 = ax.legend(*scatter.legend_elements(), loc='upper left', ncol=3, title="Allowed per Course")
+# # ax.add_artist(legend1)
+# # Add colorbar to show the mapping of colors to allowed_per_course values
+# # colorbar = plt.colorbar(scatter, label='Allowed per Course')
 #
 # # Save the figure
-# plt.savefig('3d-scatter-sir.png')
+# plt.savefig('10act-3d-scatter-si-model-uai.png')
 #
-# # Create an instance of PrettyTable
-# pt = PrettyTable()
 #
-# # Define column headers
-# pt.field_names = ["Current Infected", "Allowed Per Course", "Community Risk", "Infected Students"]
-#
-# # Add rows to the table
-# for row in table:
-#     pt.add_row(row)
-#
-# # Optionally, you can set the alignment and other formatting options
-# pt.align = "r"
-# pt.float_format = "0.2"
-#
-# # Print the table
-# table_str = pt.get_string()
-# with open("table-sir.txt", "w") as file:
-#     file.write(table_str)
+# #
+# # # Prepare data
+# # current_infected_data = [row[0] for row in table]
+# # allowed_per_course_data = [row[1] for row in table]
+# # community_risk_data = [row[2] for row in table]
+# # infected_students_data = [row[3] for row in table]
+# #
+# # # Create a 3D scatter plot
+# # fig = plt.figure()
+# # ax = fig.add_subplot(111, projection='3d')
+# #
+# # # Scatter plot
+# # scatter = ax.scatter(current_infected_data, allowed_per_course_data, community_risk_data,
+# #                      c=infected_students_data, cmap='plasma', marker='o')
+# #
+# # # Adding labels and title
+# # ax.set_xlabel('Current Infected')
+# # ax.set_ylabel('Allowed Per Course')
+# # ax.set_zlabel('Community Risk')
+# # plt.title('Infected Students based on approximated sir model')
+# #
+# # # Adding a color bar
+# # cbar = plt.colorbar(scatter, ax=ax)
+# # cbar.set_label('Infected Students')
+# #
+# # # Save the figure
+# # plt.savefig('3d-scatter-sir.png')
+# #
+# # # Create an instance of PrettyTable
+# # pt = PrettyTable()
+# #
+# # # Define column headers
+# # pt.field_names = ["Current Infected", "Allowed Per Course", "Community Risk", "Infected Students"]
+# #
+# # # Add rows to the table
+# # for row in table:
+# #     pt.add_row(row)
+# #
+# # # Optionally, you can set the alignment and other formatting options
+# # pt.align = "r"
+# # pt.float_format = "0.2"
+# #
+# # # Print the table
+# # table_str = pt.get_string()
+# # with open("table-sir.txt", "w") as file:
+# #     file.write(table_str)
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the list of alpha values
-alphas=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
+alphas=(0.1 0.2 0.3 0.4 0.5 0.6 0.9)
 
 # Loop through each alpha value
 for alpha in "${alphas[@]}"
